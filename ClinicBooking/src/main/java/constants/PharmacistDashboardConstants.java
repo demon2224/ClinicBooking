45f6c8ca7e93af7254c5df21cc5a9c364e569014
Khangnghien <vuminhkhang2004@gmail.com>
2025-10-10 15:52:40 +0700

    feat (constants): add class for store constant of controller
    - Created PharmacistDashBoardConstants for storing url, notification, constants.

---
+++ ClinicBooking/src/main/java/constants/PharmacistDashboardConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package constants;
+
+/**
+ *
+ * @author Vu Minh Khang - CE191371
+ */
+public class PharmacistDashboardConstants {
+    
+    // Context path of the web.
+    public static final String CONTEXT_PATH = "/ClinicBooking";
+    
+    // URL using in Pharmacist Dashboard
+    public static final String PHARMACIST_DASHBOARD_URL =  "/WEB-INF/pharmacist/PharmacistDashboard.jsp";
+    
+    
+}
