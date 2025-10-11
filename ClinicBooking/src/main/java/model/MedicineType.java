ac0e54a597f06d086e48ada4d1f98a6a3cbcb634
Khangnghien <vuminhkhang2004@gmail.com>
2025-10-11 23:32:56 +0700

    docs (model): add comment
    - Added comment to all attribute and method in Medicine model.
    - Added comment to all attribute and method in MedicineStockTransaction model.
    - Added comment to all attribute and method in MedicineType model.
    - Added comment to all attribute and method in MedicineViewModel model.

--- ClinicBooking/src/main/java/model/MedicineType.java
+++ ClinicBooking/src/main/java/model/MedicineType.java
@@ -10,29 +10,61 @@ package model;
  */
 public class MedicineType {
     
+    // The unique identifier for the medicine type
     private int medicineId;
+    
+    // The name/description of the medicine type
     private String medicineType;
 
+    /**
+     * Default constructor for MedicineType.
+     */
     public MedicineType() {
     }
 
+    /**
+     * Constructs a MedicineType with specified ID and type name.
+     *
+     * @param medicineId   The unique identifier for the medicine type
+     * @param medicineType The name/description of the medicine type
+     */
     public MedicineType(int medicineId, String medicineType) {
         this.medicineId = medicineId;
         this.medicineType = medicineType;
     }
 
+    /**
+     * Gets the medicine type ID.
+     *
+     * @return The medicine type ID
+     */
     public int getMedicineId() {
         return medicineId;
     }
 
+    /**
+     * Sets the medicine type ID.
+     *
+     * @param medicineId The medicine type ID to set
+     */
     public void setMedicineId(int medicineId) {
         this.medicineId = medicineId;
     }
 
+    /**
+     * Gets the medicine type name.
+     *
+     * @return The medicine type name
+     */
     public String getMedicineType() {
         return medicineType;
     }
 
+    /**
+     * Sets the medicine type name.
+     *
+     * @param medicineType The medicine type name to set
+     */
     public void setMedicineType(String medicineType) {
         this.medicineType = medicineType;
     }
