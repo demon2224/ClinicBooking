c5b90886d9fe85fefb4d8cdb8199df4bd6d8664a
Nguyễn Minh Khang <khangnm.ce190728@gmail.com>
2025-10-14 15:55:54 +0700

    feat(Avatar): separate avatar processing logic
    - Create AvatarValidate in validate.
    - Create AvatarHandler in utils.
    - Create AvatarConstants in constants.

---
+++ ClinicBooking/src/main/java/constants/AvatarConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package constants;
+
+/**
+ *
+ * @author Nguyen Minh Khang - CE190728
+ */
+public class AvatarConstants {
+
+    // Avatar path configuration
+    public static final String DEFAULT_AVATAR = "assests/img/0.png";
+    public static final String AVATAR_BASE_PATH = "assests/img/";
+    public static final String AVATAR_PATH_PREFIX = "assests/";
+}
